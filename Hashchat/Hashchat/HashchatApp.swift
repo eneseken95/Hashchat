72884e5916d2e33917e679a2bbec08c811524d6d
Enes Eken <144843964+eneseken95@users.noreply.github.com>
2025-10-11 13:31:59 +0300

    Hashchat

---
+++ Hashchat/Hashchat/HashchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashchatApp.swift
+//  Hashchat
+//
+//  Created by Enes Eken 2 on 11.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct HashchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
